54d54c017c52a8b3d134efceef25f9e13cf95be9
Slava <ucheba00@gmail.com>
2019-07-09 14:18:59 -0600

    fetch and output github users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
   render() {
     return (
       <div className='App'>
-        <h1>Hello yo</h1>
+        <Navbar />
+        <div className='container'>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import spinner from './spinner.gif';
+
+const Spinner = () => {
+  return (
+    <React.Fragment>
+      <img
+        src={spinner}
+        alt=''
+        style={{ width: '200px', margin: 'auto', display: 'block' }}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Spinner;
