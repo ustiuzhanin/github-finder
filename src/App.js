f6076b4836f9b9af534f5785cc6501b5f847aa97
Slava <ucheba00@gmail.com>
2019-07-12 19:20:09 -0600

    prepare for deploying to netlify

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './contex/github/GithubState';
 import AlertState from './contex/alert/AlertState';
@@ -21,18 +21,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <React.Fragment>
-                      <Search />
-                      <Users />
-                    </React.Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Search from '../users/Search';
+import Users from '../users/Users';
+
+const Home = () => (
+  <React.Fragment>
+    <Search />
+    <Users />
+  </React.Fragment>
+);
+
+export default Home;
